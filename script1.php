654735321c0987761260180ca26441444a9f241b
pvhole <pvhole@gmail.com>
2016-10-18 04:51:24 -0400

    Update script1.php

--- script1.php
+++ script1.php
@@ -11,4 +11,35 @@
 <!-- Latest compiled JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 </head>
+  <body>
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Assignment</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="list.php">list</a></li>
+      <li><a href="firstAss1.php">Ass1</a></li>
+      <li><a href="ass2.php">Ass2</a></li>
+      <li><a href="ass3.php">Ass3</a></li>
+    </ul>
+  </div>
+</nav>
+
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Assignment</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="list.php">list</a></li>
+      <li><a href="firstAss1.php">Ass1</a></li>
+      <li><a href="ass2.php">Ass2</a></li>
+      <li><a href="ass3.php">Ass3</a></li>
+    </ul>
+  </div>
+</nav>
+</body>
 </html>
