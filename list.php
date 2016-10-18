ed351672e90e630280c7c91e26bc7310c07f5cb3
pvhole <pvhole@gmail.com>
2016-10-18 05:23:17 -0400

    Update list.php

--- list.php
+++ list.php
@@ -19,7 +19,7 @@ try
     $query1->execute();
     //$result = $query1->fetch(PDO::FETCH_ASSOC);
    ?>
-  <div class="container">
+  <div class="container"style="margin-top:50px">
  <center> <h1>RECORD</h1></center>
            
   <table class="table table-striped">
