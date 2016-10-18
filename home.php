344b4bffa3831784151957e338830b1f720dc876
pvhole <pvhole@gmail.com>
2016-10-18 13:17:56 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>home</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body style="height:1500px">
+
+<nav class="navbar navbar-inverse navbar-fixed-top">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Assignment</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="list.php">list</a></li>
+      <li><a href="firstAss1.php">Ass1</a></li>
+      <li><a href="ass2.php">Ass2</a></li>
+      <li><a href="ass3.php">Ass3</a></li>
+    </ul>
+  </div>
+</nav>
+  
+
+  
+<div class="container" style="margin-top:50px">
+  <center><h1>Write SQL, Print HTML Table</h1></center>
+  <div class="row">
+    <div class="col-md-4">
+      <p>1. Select all transactions where quantity > 100 and price > 500.</p>
+      <p>2. Prepare a report of transactions by Location ID
+Eg:
+
+Location ID, Number of Transactions
+1     1500
+2    3000.</p>    
+    </div>
+    <div class="col-md-4"> 
+      <p>3) Prepare a report of transactions where Quantity is 0, or Price is 0, or Value is 0.</p>
+     </div>
+    <div class="col-md-4"> 
+      <p>Display 4 pages:.</p>
+      <p>Select any 5 attributes (including id)</p> 
+      <p>1. List of first 100 transactions in HTML Table (list.php</p>
+      <p>2. Link to view, edit and delete each transaction </p>
+      <p>3. View page which displays all attributes of transactions (eg: view.php?id=1001)</p>
+      <p>4. Edit one transaction and update the same using a HTML Form (eg: edit.php?id=1001)</p>
+      <p>5. Delete a transaction (eg: delete.php?id=1001)</p>
+    </div>
+  </div>
+</div>
+</div>
+
+<nav class="navbar navbar-inverse navbar-fixed-bottom">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">Assignment</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="home.php">Home</a></li>
+      <li><a href="list.php">list</a></li>
+      <li><a href="firstAss1.php">Ass1</a></li>
+      <li><a href="ass2.php">Ass2</a></li>
+      <li><a href="ass3.php">Ass3</a></li>
+    </ul>
+  </div>
+</nav>
+</body>
+</html>
+
